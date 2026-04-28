f82dbc3019d6b0b24de8c320d88fcbc3949626da
Thomas Bobin <tbobin@geotec.fr>
2026-04-28 13:57:09 +0200

    [FEAT] Continuation of slider creation

--- src/app/characters/character/character.component.ts
+++ src/app/characters/character/character.component.ts
@@ -10,6 +10,7 @@ import { CapitalizePipe } from "../../../utilities/pipes/capitalizePipe";
 import { TabComponent } from "../../../components/tab/tab.component";
 import { TabItemComponent } from "../../../components/tab/tab-item/tab-item.component";
 import { SliderComponent } from "../../../components/slider/slider.component";
+import { FormsModule } from '@angular/forms';
 
 export const MONDSTADT_LANDSCAPE_NUMBER = 4;
 export const LIYUE_LANDSCAPE_NUMBER = 5;
@@ -32,7 +33,7 @@ export const VISION_IMAGE_MAP: Partial<Record<ProjectClass.Local.VisionType, str
 
 @Component({
   selector: 'app-character',
-  imports: [CommonModule, CapitalizePipe, TabComponent, TabItemComponent, SliderComponent],
+  imports: [CommonModule, CapitalizePipe, TabComponent, TabItemComponent, SliderComponent, FormsModule],
   templateUrl: './character.component.html',
   styleUrl: './character.component.scss',
   standalone: true
@@ -55,6 +56,8 @@ export class CharacterComponent implements OnInit, OnDestroy{
   public slideDuration = 10;
   private slideTimer: any;
   public skillTalentTypeList = ProjectClass.Local.SkillTalentTypeList;
+
+  public testValue = 4;
 
   constructor(
     public charactersService : CharactersService,
