b2641e5bf89324e794815b01d9e6fa339ed024c8
Thomas Bobin <tbobin@geotec.fr>
2026-04-29 14:02:28 +0200

    [FIX] Rework of slider

--- src/components/slider/slider.component.ts
+++ src/components/slider/slider.component.ts
@@ -1,68 +1,17 @@
-import { Component, EventEmitter, forwardRef, Input, Output } from '@angular/core';
-import {CdkDrag} from '@angular/cdk/drag-drop';
-import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
-
-export enum BallSize {
-  BIG = "big",
-  NORMAL = "normal",
-  SMALL = "small"
-}
-
-export enum SliderTheme {
-  LIGHT = "light",
-  DARK = "dark"
-}
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'slider-component',
-  imports: [CdkDrag],
+  imports: [FormsModule],
   templateUrl: './slider.component.html',
-  styleUrl: './slider.component.scss',
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => SliderComponent),
-      multi: true
-    }
-  ]
+  styleUrl: './slider.component.scss'
 })
 
-export class SliderComponent implements ControlValueAccessor{
-  @Input() ballSize : BallSize = BallSize.BIG
-  @Input() sliderTheme : SliderTheme = SliderTheme.DARK
-  @Input() step : number = 0;
-  @Input() min : number = 0;
-  @Input() max :  number = 0;
+export class SliderComponent {
+  @Input() min: number = 0;
+  @Input() max: number = 10;
 
-  private totalAnchor: number = 0;
-  public anchors: number[] = [];
-
-  private currentValue : number = 0;
-
-  ngOnInit(): void {
-    this.totalAnchor = (this.max + 1) - this.min;
-    this.anchors = Array.from({ length: this.totalAnchor }, (_, i) => this.min + i);
-  }
-  
-  onChange = (value: number) => {};
-  onTouched = () => {};
-
-  writeValue(value: number): void {
-    this.currentValue = value;
-  }
-  
-  registerOnChange(fn: any): void {
-    this.onChange = fn;
-  }
-
-  registerOnTouched(fn: any): void {
-    this.onTouched = fn;
-  }
-
-  setDisabledState?(isDisabled: boolean): void {
-  }
-
-  public test(): void {
-    console.log("shfhjsdgfsd")
-  }
+  value: number = 0;
+  tooltipPosition: string = '0%';
 }
